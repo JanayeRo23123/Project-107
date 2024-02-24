87c10570085dd6f1bbd34d951fcdb67c23dcff17
JanayeRo23123 <151698192+JanayeRo23123@users.noreply.github.com>
2024-02-24 00:27:57 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Catalog from './pages/catalog';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 
+
 function App() {
   return (
     <div className="App">
+
       <Navbar/>
       <Catalog/>
       <Footer/>
